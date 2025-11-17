b59b6fe35adb9b301c9378a242393bbce6d59947
KrishnaPatel <Krishnapatel@gmail.com>
2025-11-17 11:36:18 +0530

    donelastone

--- Backend/server.js
+++ Backend/server.js
@@ -5,24 +5,39 @@ import "dotenv/config";
 
 let app = express();
 
-// Allowed origins
+// // Allowed origins
+// const allowedOrigins = [
+//   "https://up-skillx.vercel.app", // Vercel frontend
+//   "http://localhost:5173",        // Local Vite dev server
+//   "http://localhost:5174"               
+// ];
+
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin like Postman
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }));
+// FIXED CORS CODE
 const allowedOrigins = [
-  "https://up-skillx.vercel.app/", // Vercel frontend
-  "http://localhost:5173"              // Local Vite dev server
+  "https://up-skillx.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5174",
 ];
 
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin like Postman
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
 }));
+
+app.options("*", cors());
+
 
 
 
